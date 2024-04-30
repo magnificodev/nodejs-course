ff87dc7322f96424d4e72589ccfd1892ca03daf0
magnificodev <magnificodev8@gmail.com>
2024-04-30 21:06:33 +0700

    Day 04 - First Commit

---
+++ day03/logEvents.js
@@ -0,0 +1,24 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (logMsg) => {
+  const dateTime = `${format(new Date(), "HH:mm:ss - dd/MM/yyyy")}`;
+  const logItem = `[${dateTime}]\t${uuid()}\t${logMsg}\n`;
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs/"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs/"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "eventLog.txt"),
+      `${logItem}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = logEvents;
