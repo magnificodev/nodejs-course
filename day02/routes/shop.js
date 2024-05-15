2ce1b74f415db956a2cf3d4d41c924b637c26d8d
magnificodev <magnificodev8@gmail.com>
2024-05-15 21:37:29 +0700

    Static Middleware ExpressJS

---
+++ day02/routes/shop.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require('path')
+const router = express.Router();
+
+const rootDir = require("../utils/path")
+
+// GET -> /
+router.get("/", (req, res) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+module.exports = router;
